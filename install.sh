3af695ee9ce1dd857b0548dd9c76b4e605d1501a
W1LMS <36493912+W1LMS@users.noreply.github.com>
2020-07-03 13:50:01 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install -y python3-twisted
+
+cp ./n0mjs710/* /opt/
+cp ./systemd/hblink.service /lib/systemd/system/
+
+cd /opt/dmr_utils3
+./install.sh
+
+cd /opt/HBlink3
+./install.sh
+
+cp hblink-SAMPLE.cfg hblink.cfg
+cp rules_SAMPLE.py rules.py
+
+echo "Edit hblink.cfg and rules.py. Once done, test it out with "python3 bridge.py"
+echo "If all goes well, you can enable and start the service by typing:"
+echo "sudo systemctl enable hblink"
+echo "sudo systemctl start hblink"
