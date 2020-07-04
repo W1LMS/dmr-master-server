042eef0618475bd6ee7aad9dd25f141bdbe2b651
W1LMS <36493912+W1LMS@users.noreply.github.com>
2020-07-03 19:35:00 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,16 @@ cp -r ./n0mjs710/* /opt/
 cp -r ./systemd/hblink.service /lib/systemd/system/
 
 cd /opt/dmr_utils3
-./install.sh
+. ./install.sh
 
 cd /opt/HBlink3
-./install.sh
+. ./install.sh
 
 cp hblink-SAMPLE.cfg hblink.cfg
 cp rules_SAMPLE.py rules.py
 
-echo "Edit hblink.cfg and rules.py. Once done, test it out with "python3 bridge.py"
+echo "Edit hblink.cfg and rules.py. Once done, test it out by typing:"
+echo "python3 bridge.py"
 echo "If all goes well, you can enable and start the service by typing:"
-echo "sudo systemctl enable hblink"
-echo "sudo systemctl start hblink"
+echo "systemctl enable hblink"
+echo "systemctl start hblink"
