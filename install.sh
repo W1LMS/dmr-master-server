419f066d2821d8d9102c8b11ed8495cebdbafd47
W1LMS <36493912+W1LMS@users.noreply.github.com>
2020-12-24 20:10:02 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ python3 -m pip install --upgrade pip
 python3 -m pip install wheel
 
 cp -r ./n0mjs710/* /opt/
-cp -r ./systemd/hblink.service /lib/systemd/system/
+cp ./systemd/hblink.service /lib/systemd/system/
 
 cd /opt/dmr_utils3/
 . ./install.sh
@@ -20,4 +20,4 @@ cp ./rules_SAMPLE.py ./rules.py
 
 clear
 
-printf "\e[0m\e[1;33mEdit hblink.cfg and rules.py. Once done, test it out by typing:\e[0m\n\t\e[1;36mpython3 bridge.py\e[0m\n\n\e[1;33mIf all goes well, you can enable and start the service by typing:\e[0m\n\t\e[1;36msystemctl enable hblink\e[0m\n\t\e[1;36msystemctl start hblink\e[0m\n\n"
+printf "\e[0m\e[1;33mEdit hblink.cfg and rules.py. Once done, test it out by typing:\e[0m\n\t\e[1;36mpython3 bridge.py\e[0m\n\n\e[1;33mIf all goes well, you can enable and start the service by typing:\e[0m\n\t\e[1;36msystemctl enable hblink\e[0m\n\n\e[0m\e[1;33mThen reboot your device. You can check the logs at:\e[0m\n\t\e[1;36m/tmp/hblink.log\e[0m\n\n"
